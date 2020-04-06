d02e4a390457504457adad2e0cfaefc84592b6d4
cristianyairmiranda08 <62486189+cristianyairmiranda08@users.noreply.github.com>
2020-04-05 20:51:03 -0500

    Practica Herencia

---
+++ Dog.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author crist
+ */
+public class Dog {
+	private String name;
+	private double weight;
+	private String colour;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public void printToConsole() {
+		System.out.print(" name: " + getName());
+		System.out.print(" colour: " + this.colour);
+		System.out.println(" weight: " + this.weight);
+	}
+}
+
+ 
---
+++ Dog2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author crist
+ */
+public class Dog2 extends Dog {
+
+	private double portion;
+
+	public double getPortion() {
+		return portion;
+	}
+        
+	public void setPortion(double portion) {
+		this.portion = portion;
+	}
+}
